1debc96dc7b185ff1bc51cb45c3325ea49d45638
Graham Hall <ghall89@me.com>
2021-03-04 13:47:31 -0500

    initial functionality for searching and displaying weather

---
+++ assets/js/app.js
@@ -0,0 +1,62 @@
+const searchFieldEl = document.querySelector("#search-field");
+const searchBtnEl = document.querySelector("#search-btn");
+const weatherContentEl = document.querySelector("#weather-content");
+
+// Get todays weather data based on search query
+const getWeather = searchQuery => {
+	fetch("http://api.openweathermap.org/data/2.5/weather?q=" + searchQuery + "&units=imperial&appid=1433428c54ebfc06c679ee5966e161bb")
+		.then(function(response) {
+			if (response.ok) {
+				response.json()
+					.then(function(data) {
+						currentWeather(data);
+					});
+			} else {
+				alert("Failed to get data from API");
+			}
+		});
+};
+
+
+// Display weather data for today
+const currentWeather = data => {
+	const weatherCard = document.createElement("div");
+	weatherCard.classList = "card m-3";
+	const cardContentEl = document.createElement("div");
+	cardContentEl.classList = "card-body";
+	const cardTitleEl = document.createElement("h5");
+	cardTitleEl.classList = "card-title";
+	cardTitleEl.textContent = data.name + " (" + moment().format("M/D/yyyy") + ")";
+	const tempTextEl = document.createElement("p");
+	tempTextEl.classList = "text-secondary";
+	tempTextEl.textContent = "Temperature: " + data.main.temp + "°F";
+	const humidityTextEl = document.createElement("p");
+	humidityTextEl.classList = "text-secondary";
+	humidityTextEl.textContent = "Humidity: " + data.main.humidity + "%";
+	const windTextEl = document.createElement("p");
+	windTextEl.classList = "text-secondary";
+	windTextEl.textContent = "Wind Speed: " + data.wind.speed + " MPH";
+	const uvTextEl = document.createElement("p");
+	uvTextEl.classList = "text-secondary";
+	uvTextEl.innerHTML = "UV Index <span class='badge bg-danger'>null</span>";
+	
+	weatherContentEl.appendChild(weatherCard);
+	weatherCard.appendChild(cardContentEl);
+	cardContentEl.appendChild(cardTitleEl);
+	cardContentEl.appendChild(tempTextEl);
+	cardContentEl.appendChild(humidityTextEl);
+	cardContentEl.appendChild(windTextEl);
+	cardContentEl.appendChild(uvTextEl);
+}
+
+
+searchBtnEl.addEventListener("click", function() {
+	event.preventDefault();
+	
+	weatherContentEl.innerHTML = "";
+
+	const searchQuery = searchFieldEl.value;
+
+	getWeather(searchQuery);
+
+});
